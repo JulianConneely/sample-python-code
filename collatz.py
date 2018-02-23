3c85383a05a11a09343a9542a804c3d158879f0b
Conneely <jconneely@hpe.com>
2018-02-23 21:33:02 +0000

    Uploading collatz.py as Exercise 3

--- collatz.py
+++ collatz.py
@@ -1,6 +1,13 @@
-# Julian Conneely, 2018-09-02
+# Julian Conneely, 2018-23-02
 # The Collatz conjecture program
 
-n = 17
+n = int(input("Please enter an integer: "))
+while n != 1:       # while n is not equal to 1 do this
+  if (n % 2 == 0):  # this is an even number
+    n = n // 2      # n should be divided by 2 with // indicating no decimal points
+    print (n)       # print n in the integrated terminal
+  elif n % 2 != 0:  #this is an odd number
+    n = (n * 3) + 1 # n should be multiplied by 2 with and add 1
+    print (n)       # print n in the integrated terminal 
 
-while...wip
+print ("the final value of n is 1")
